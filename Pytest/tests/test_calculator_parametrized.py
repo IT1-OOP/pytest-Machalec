ce621fc63c7ffbfdef9a8ff0689cd3c2e990b8fb
Machalec <94050101+Machalec@users.noreply.github.com>
2025-03-06 13:16:59 +0100

    parametrized_done

--- Pytest/tests/test_calculator_parametrized.py
+++ Pytest/tests/test_calculator_parametrized.py
@@ -25,5 +25,62 @@ def test_add_2():
 def test_add_parametrized(a, b, expected):
     assert calculator.add(a, b) == expected
 
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 3),
+        (0, 5, 5),
+        (-2, 9, 7),
+        (5, -8, -3),
+    ],
+)
+def test_add_wrong_parametrized(a, b, expected):
+    assert calculator.add_wrong(a, b) == expected
 
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 3),
+        (0, 5, 5),
+        (-2, 9, 7),
+        (5, -8, -3),
+    ],
+)
+def test_subtract_parametrized(a, b, expected):
+    assert calculator.subtract(a, b) == expected
 
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 2),
+        (0, 5, 0),
+        (2, 9, 18),
+        (5, 8, 40),
+    ],
+)
+def test_multiply_parametrized(a, b, expected):
+    assert calculator.multiply(a, b) == expected
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 2),
+        (0, 5, 0),
+        (2, 9, 18),
+        (5, 8, 40),
+    ],
+)
+def test_multiply_wrong_parametrized(a, b, expected):
+    assert calculator.multiply_wrong(a, b) == expected
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (4, 2, 2),
+        (8, 4, 2),
+        (20, 5, 4),
+        (18, 2, 9),
+    ],
+)
+def test_divide_parametrized(a, b, expected):
+    assert calculator.divide(a, b) == expected
